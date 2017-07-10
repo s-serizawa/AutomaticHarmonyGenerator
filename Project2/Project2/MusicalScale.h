92fded9784585f939690fd0df760b8c3bc587a78
Nodepuiw <endonoriaki.3314@gmail.com>
2017-07-10 20:41:08 +0900

    Organize each scale note as an array

--- Project2/Project2/MusicalScale.h
+++ Project2/Project2/MusicalScale.h
@@ -11,8 +11,9 @@ private:
 	int mediant;        //---- 中音(3度上)、ミ
 	int subdominant;   //---- 下属音(完全4度上)、ファ
 	int dominant;     //---- 属音(完全5度上)、ソ
-	int submediant;  //---- 下中音(完全6度上)、ラ
+	int submediant;  //---- 下中音(6度上)、ラ
 	int leadingtone;//----　導音(短2度下)、シ
+	std::vector<int> scalenotes; //---- 配列管理したほうがよさそう
 
 	bool is_chordreading_success;
 
@@ -33,5 +34,8 @@ public:
 
 	//---- コードから，コードの乗っているスケールを判定し，クラスMusicalScaleのメンバ変数に代入
 	void setScaleAccordingToChord(const std::string& chord);
+
+	//---- 与えられたコードネーム，音高，ハモリの度数から，よさげなハモリ音を返す関数
+	int returnHarmonicTone(const std::string& chord, const int& note, const int& degree);
 
 };
--- Project2/Project2/harmony_generator.cpp
+++ Project2/Project2/harmony_generator.cpp
@@ -8,6 +8,7 @@ MusicalScale::MusicalScale()
 	dominant(0),
 	submediant(0),
 	leadingtone(0),
+	scalenotes(7,0),
 	is_chordreading_success(true)
 {
 }
@@ -29,19 +30,19 @@ void MusicalScale::setScaleAccordingToChord(const std::string& chord) {
 	{
 		switch (chord[0])
 		{
-		case 'A': tonic = NOTE_RA;
+		case 'A': tonic = NOTE_RA; scalenotes[0] = NOTE_RA;
 			break;
-		case 'B': tonic = NOTE_SI;
+		case 'B': tonic = NOTE_SI; scalenotes[0] = NOTE_SI;
 			break;
-		case 'C': tonic = NOTE_DO;
+		case 'C': tonic = NOTE_DO; scalenotes[0] = NOTE_DO;
 			break;
-		case 'D': tonic = NOTE_RE;
+		case 'D': tonic = NOTE_RE; scalenotes[0] = NOTE_RE;
 			break;
-		case 'E': tonic = NOTE_MI;
+		case 'E': tonic = NOTE_MI; scalenotes[0] = NOTE_MI;
 			break;
-		case 'F': tonic = NOTE_FA;
+		case 'F': tonic = NOTE_FA; scalenotes[0] = NOTE_FA;
 			break;
-		case 'G': tonic = NOTE_SO;
+		case 'G': tonic = NOTE_SO; scalenotes[0] = NOTE_SO;
 			break;
 		default:
 			is_chordreading_success = false;
@@ -57,25 +58,31 @@ void MusicalScale::setScaleAccordingToChord(const std::string& chord) {
 			dominant = tonic + 7;
 			submediant = tonic + 9;
 			leadingtone = tonic + 11;
+			scalenotes[1] = scalenotes[0] + 2;
+			scalenotes[2] = scalenotes[0] + 4;
+			scalenotes[3] = scalenotes[0] + 5;
+			scalenotes[4] = scalenotes[0] + 7;
+			scalenotes[5] = scalenotes[0] + 9;
+			scalenotes[6] = scalenotes[0] + 11;
 		}
 	}
 	else
 		if (chord.length() == 2) //---- 絶対に短調になる前提で書いてる
 			switch (chord[0])
 			{
-			case 'A': tonic = NOTE_RA;
+			case 'A': tonic = NOTE_RA; scalenotes[0] = NOTE_RA;
 				break;
-			case 'B': tonic = NOTE_SI;
+			case 'B': tonic = NOTE_SI; scalenotes[0] = NOTE_SI;
 				break;
-			case 'C': tonic = NOTE_DO;
+			case 'C': tonic = NOTE_DO; scalenotes[0] = NOTE_DO;
 				break;
-			case 'D': tonic = NOTE_RE;
+			case 'D': tonic = NOTE_RE; scalenotes[0] = NOTE_RE;
 				break;
-			case 'E': tonic = NOTE_MI;
+			case 'E': tonic = NOTE_MI; scalenotes[0] = NOTE_MI;
 				break;
-			case 'F': tonic = NOTE_FA;
+			case 'F': tonic = NOTE_FA; scalenotes[0] = NOTE_FA;
 				break;
-			case 'G': tonic = NOTE_SO;
+			case 'G': tonic = NOTE_SO; scalenotes[0] = NOTE_SO;
 				break;
 			default:
 				is_chordreading_success = false;
@@ -90,16 +97,35 @@ void MusicalScale::setScaleAccordingToChord(const std::string& chord) {
 		   //---- 自然短音階で書いてます
 			supertonic = tonic + 2;
 			mediant = tonic + 3;
-			subdominant = tonic + 4;
+			subdominant = tonic + 5;
 			dominant = tonic + 7;
 			submediant = tonic + 8;
 			leadingtone = tonic + 10;
+			scalenotes[1] = scalenotes[0] + 2;
+			scalenotes[2] = scalenotes[0] + 3;
+			scalenotes[3] = scalenotes[0] + 5;
+			scalenotes[4] = scalenotes[0] + 7;
+			scalenotes[5] = scalenotes[0] + 8;
+			scalenotes[6] = scalenotes[0] + 10;
 		}
 		else
 		{
 			std::cout << "the process for this chord name is undefined." << std::endl;
 		}
 	}
+}
 
+int MusicalScale::returnHarmonicTone(const std::string& chord, const int& note, const int& degree) {
+	setScaleAccordingToChord(chord);
+	for (int i = 0; i < scalenotes.size(); ++i)
+	{
+		if (scalenotes[i] == note) //---- スケールに乗っているとき。どっちかには合致するはず
+		{
+
+		}
+		else if (scalenotes[i] == note + 1) //---- スケールに乗っていないとき。どっちかには合致するはず
+		{
+
+		}
+	}
 };
-
